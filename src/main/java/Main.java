04d44d588781fbd61fe3c3321d2818cd7c8d96ef
YatskivNazar <blogyatskiv@gmail.com>
2017-04-10 13:06:58 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -19,7 +24,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> {
+      RelativisticModel.select();
+      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+      return "E=mc^2: 12 GeV = " + m.toString();
+    });
 
     get("/", (request, response) -> {
         Map<String, Object> attributes = new HashMap<>();
